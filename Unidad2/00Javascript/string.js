676788ec78545e09f99fd0cee26483bbd89ae659
Anderson <andersonfabi3@gmail.com>
2026-06-12 13:08:38 -0500

    Trabajajo en clase 12/jun/2026

---
+++ Unidad2/00Javascript/string.js
@@ -0,0 +1,19 @@
+
+let string1 = 'Hola, mundo!'
+let string2 = "JavaScript es para programadores"
+let string3 = `${string1} feliz`
+let string4 = string1 + ' ' + string2
+
+console.log(string1)
+console.log(string2)
+console.log(string3)
+console.log(string4)
+
+let frase = 'JavaScript es un lenguaje de programación'
+console.log(frase.length) // longitud de la cadena
+console.log(frase.toUpperCase()) // convierte a mayúsculas
+console.log(frase.toLowerCase()) // convierte a minúsculas
+console.log(frase.toLocaleLowerCase()) // convierte a minúsculas según la configuración regional
+console.log(frase.toLowerCase()) // convierte a minúsculas
+console.log(frase.substring(29, 42)) // extrae una parte de la cadena
+
