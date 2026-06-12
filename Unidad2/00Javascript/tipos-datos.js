d7d3e8eb7db92d03d9450d57a3a378156458b990
Anderson <andersonfabi3@gmail.com>
2026-06-12 13:17:04 -0500

    actualizacion

---
+++ Unidad2/00Javascript/tipos-datos.js
@@ -0,0 +1,35 @@
+//PRIMITIVOS
+let nombre = "Anderson" //String
+
+//NUMERICOS
+let edad = 30
+
+//BOOLEANOS
+let esMayorDeEdad = true
+
+//NULL
+let noHayValor = null
+
+//UNDEFINED
+let noDefinido = undefined
+
+//SYMBOL VALOR UNICO
+let simboloUnico = Symbol('unico')
+
+//DATOS GIANTES BIGINT
+let numeroGigante = 2n
+
+//COMPLETOS
+let carro = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    año: 2020
+}
+
+//ARRAYS
+let frutas = ['manzana', 'banana', 'naranja']
+
+//FUNCIONES
+function saludar() {
+}
+
---
+++ Unidad2/PilaPaquete.java
@@ -0,0 +1,1 @@
+// PilaPaquete.java
